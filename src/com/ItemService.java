d44e1c1137d8bb2246877fe0efb6d1de391f7df1
haritha1012 <haritha981012@gmail.com>
2020-04-19 13:15:43 +0530

    test

---
+++ src/com/ItemService.java
@@ -0,0 +1,30 @@
+package com;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Item;
+
+@Path("/Items")
+public class ItemService {
+	Item itemObj = new Item();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems() {
+		return "Hello";
+	}
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems() {
+		return itemObj.readItems();
+	}
+
+	
+}
